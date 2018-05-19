d3c809168fc06b2d8baa699ab48920049a107014
Carlos Franco <c.franco@udc.es>
2018-05-19 19:00:25 +0200

    doing shit

--- seguros/src/main/java/es/udc/fi/lbd/monuzz/id/seguros/daos/ClienteDAOHibImpl.java
+++ seguros/src/main/java/es/udc/fi/lbd/monuzz/id/seguros/daos/ClienteDAOHibImpl.java
@@ -38,6 +38,8 @@ public class ClienteDAOHibImpl implements ClienteDAO {
 	public void remove(Cliente meuCliente) {
 		if (meuCliente.getIdCliente() == null)
 			throw new RuntimeException("Cliente Invalido: O Cliente non pode ser nulo");
+		if (!meuCliente.getSegurosSubscritos().isEmpty())
+			meuCliente.getSegurosSubscritos().clear();;
 		sessionFactory.getCurrentSession().delete(meuCliente);
 		
 	}
